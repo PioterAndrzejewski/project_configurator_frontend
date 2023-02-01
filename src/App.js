918a48e165763da8a2e1b7ae574bc2d50ddece1e
PioterAndrzejewski <109315248+PioterAndrzejewski@users.noreply.github.com>
2023-02-01 15:39:46 +0100

    First commit - setup a project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	// return (
+	// 	<div className="App">
+	// 		<ProjectContext>
+	// 			<Visualisation />
+	// 			<aside>
+	// 				<Logotype />
+	// 				<BuildingTypeSelector />
+	// 				<PriceDisplayer />
+	// 			</aside>
+	// 			<main className="Main">
+	// 				<Navbar />
+	// 				<CostForm />
+	// 				<SpecificationIconsBar />
+	// 				<SpecificationIconsBar />
+	// 			</main>
+	// 		</ProjectContext>
+	// 	</div>
+	// );
+
+	return (
+		<div className="App">
+			<Navbar />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import styles from "../style/sass/Navbar.module.sass";
+
+function Navbar() {
+	return (
+		<>
+			<h1 className={styles.h1}>This is my navbar</h1>
+			<ul>
+				<li>Pierwszy</li>
+				<li>Drugi</li>
+				<li>Trzeci</li>
+			</ul>
+		</>
+	);
+}
+
+export default Navbar;
